60add8bcb2138c177ebab13d6ad937442ff3d9a0
TblScripte <tegnievbilol@gmail.com>
2025-03-15 16:22:48 +0500

    shatcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite" 
+import { fileURLToPath } from 'url' // Импортируем функцию для получения пути
+import { dirname } from 'path' // Импортируем для работы с путями
+
+import tailwindcss from "@tailwindcss/vite"
+
+// Определение __dirname через ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": `${__dirname}/src`, // Используем путь с __dirname
     },
   },
 })
